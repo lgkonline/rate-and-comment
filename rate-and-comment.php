bf8022ebba115afe2764ccc2419ac62e507ac683
Lars Kliesing <larskliesing@larss-mbp.speedportentry2090126250023>
2018-08-29 20:22:49 +0200

    Upvote!

--- rate-and-comment.php
+++ rate-and-comment.php
@@ -6,15 +6,18 @@ Plugin Name: Rate And Comment
 global $rac_db_version;
 $rac_db_version = "1.0";
 
+global $wpdb;
+global $rac_table_name;
+$rac_table_name = $wpdb->prefix . "rate_and_comment";
+
 function rac_install() {
     global $wpdb;
     global $rac_db_version;
-
-    $table_name = $wpdb->prefix . "rate_and_comment";
+    global $rac_table_name;
 
     $charset_collate = $wpdb->get_charset_collate();
 
-    $sql = "CREATE TABLE $table_name (
+    $sql = "CREATE TABLE $rac_table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         post_id bigint(20) NOT NULL,
         likes mediumint(9) DEFAULT '0' NOT NULL,
@@ -31,11 +34,10 @@ register_activation_hook(__FILE__, "rac_install");
 
 function rac_install_data() {
     global $wpdb;
-
-    $table_name = $wpdb->prefix . "rate_and_comment";
+    global $rac_table_name;
 
     $wpdb->insert( 
-        $table_name, 
+        $rac_table_name, 
         array( 
             "post_id" => 1,
             "likes" => 1
@@ -49,20 +51,45 @@ function rac_content($content) {
         ?>
         <?php echo $content; ?>
 
-        <button id="rac-like">👍</button>
+        <button id="rac-like">👍 <span class="count"></span></button>
         <script>
+            var ajaxurl = "<?php echo admin_url("admin-ajax.php"); ?>";
+            var postid = <?php the_ID(); ?>;
+
             jQuery(document).ready(function($) {
                 var racLikeBtn = document.getElementById("rac-like");
-                    racLikeBtn.addEventListener("click", function() {
-                        var data = {
-                        "action": "rac_action",
-                        "whatever": 1234,
-                        "identifier": <?php the_ID(); ?>
-                    };
 
-                    jQuery.post("<?php echo admin_url("admin-ajax.php"); ?>", data, function(response) {
-                        alert("Got this from the server: " + response);
-                    });
+                function updateVotes(response) {
+                    var rating = JSON.parse(response);
+                    console.log(response);
+                    console.log(rating);
+
+                    racLikeBtn.querySelector(".count").innerText = rating.likes;
+                }
+
+                jQuery.post(
+                    ajaxurl,
+                    {
+                        "action": "rac_get_rating",
+                        "post_id": postid,
+                    },
+                    function(response) {
+                        updateVotes(response);
+                    }
+                );
+
+                racLikeBtn.addEventListener("click", function() {
+                    jQuery.post(
+                        ajaxurl, 
+                        {
+                            "action": "rac_upvote",
+                            "post_id": postid
+                        }, 
+                        function(response) {
+                            updateVotes(response);
+                        }
+                    );
+                
                 });
             });
         </script>
@@ -74,17 +101,30 @@ function rac_content($content) {
 }
 add_action("the_content", "rac_content");
 
-function rac_action() {
+function rac_upvote() {
     global $wpdb;
+    global $rac_table_name;
 
-    $whatever = intval( $_POST["whatever"] );
-    $whatever += 10;
+    $post_id = $_POST["post_id"];
 
-    echo $whatever;
+    $wpdb->query("UPDATE $rac_table_name SET likes = likes + 1 WHERE post_id = '$post_id'");
+
+    rac_get_rating();
     wp_die();
 }
-add_action( "wp_ajax_rac_action", "rac_action" );
+add_action("wp_ajax_rac_upvote", "rac_upvote");
+add_action("wp_ajax_nopriv_rac_upvote", "rac_upvote");
 
 function rac_get_rating() {
-    // TO DO: Ajax result for one post
+    global $wpdb;
+    global $rac_table_name;
+
+    $post_id = $_POST["post_id"];
+
+    $row = $wpdb->get_row("SELECT * FROM $rac_table_name WHERE post_id = '$post_id'");
+
+    echo json_encode($row);
+    wp_die();
 }
+add_action("wp_ajax_rac_get_rating", "rac_get_rating");
+add_action("wp_ajax_nopriv_rac_get_rating", "rac_get_rating");
